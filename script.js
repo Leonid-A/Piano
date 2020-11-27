82a9c328095df41f0a0d3d563f4ba38359df92cb
Leonid Arzumanyan <62353171+Leonid-A@users.noreply.github.com>
2020-11-28 01:13:20 +0400

    finish

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
-const notes = document.getElementsByClassName("item");
-const basses = document.getElementsByClassName("itemBlack");
+const keyInfo = {
+    KeyA:  "note1",
+    KeyS: "note2",
+    KeyD: "note3",
+    KeyF: "note4",
+    KeyG: "note5",
+    KeyH: "note6",
+    KeyJ: "note7",
+    KeyW: "bass1",
+    KeyE: "bass2",
+    KeyT: "bass3",
+    KeyY: "bass4",
+    KeyU: "bass5"
+};
 
+(function addTilesEVentListeners(){
+    const notes = document.getElementsByClassName("item");
+    const basses = document.getElementsByClassName("itemBlack");
+    document.addEventListener("keydown", playWithKeyboard);
+    document.addEventListener("keyup", removeClass);
 
-// const note1Mid = document.getElementById("audio1");
+    for ( let i=0; i < notes.length; i++ ){
+        notes[i].addEventListener("mousedown", getId);
+        notes[i].addEventListener("mouseup", removeClass);
 
-// const note1Mid = new Audio("Notes/note1.mp3");
+        if ( i < basses.length ){
+            basses[i].addEventListener("mousedown", getId);
+            basses[i].addEventListener("mouseup", removeClass);
+        }
+    }
+})();
 
-
-
-function playMid(event){
-    const noteMid = new Audio(`Notes/${event.target.id}.mp3`)
+function playMid(id){
+    const noteMid = new Audio(`Notes/${id}.mp3`)
     noteMid.play();
+    pressEfect(id);
 }
 
+function pressEfect(id){
+    document.getElementById(id).classList.add("active");
+}
 
-for (let i=0; i<notes.length; i++){
-    notes[i].addEventListener("click", playMid);
+function getId(event) {
+    playMid(event.target.id);
+}
 
-    if (i<basses.length){
-        basses[i].addEventListener("click", playMid);
+function removeClass(event){
+    let item;
+    if ( event.type === "mouseup" ){
+        item = event.target.id;
+    } else {
+        item = keyInfo[event.code];
+    }
+
+    if (item){
+        document.getElementById(item).classList.remove("active");
+    }
+}
+
+function playWithKeyboard(event){
+    const item = keyInfo[event.code];
+    if (item){
+        playMid(item)
     }
 }
 
